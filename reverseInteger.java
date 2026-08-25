62e49562981b4d22e7cdf5ac78fabeaa1804d032
rakeshbm7019-hue <rakeshbm.7019@gmail.com>
2026-08-25 20:33:44 +0530

    1st commit

---
+++ reverseInteger.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+           
+            if (reversed > Integer.MAX_VALUE / 10 || reversed < Integer.MIN_VALUE / 10) {
+                return 0; 
+            }
+
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.reverse(123));      
+        System.out.println(sol.reverse(-123)); 
+        System.out.println(sol.reverse(120)); 
+        System.out.println(sol.reverse(1534236469)); 
+    }
+}
