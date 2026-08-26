9c522f6143363086845c281fbc503d0a290eba83
rakeshbm7019-hue <rakeshbm.7019@gmail.com>
2026-08-26 19:44:46 +0530

    7th commit

---
+++ sumofevenelement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class sumofevenelement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Enter the element ");
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                sum+=arr[i];
+            }
+        }
+        System.out.println(" " + sum);
+    }
+}
