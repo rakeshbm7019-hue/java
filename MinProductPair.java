857a1b7d4c27653f6d8d7b7bcc15e18096a4a406
rakeshbm7019-hue <rakeshbm.7019@gmail.com>
2026-08-28 19:06:55 +0530

    12th commit

---
+++ MinProductPair.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MinProductPair {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    
+        int minProduct = Integer.MAX_VALUE;
+        int num1 = 0, num2 = 0;
+
+    
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int product = arr[i] * arr[j];
+                if (product < minProduct) {
+                    minProduct = product;
+                    num1 = arr[i];
+                    num2 = arr[j];
+                }
+            }
+        }
+
+        System.out.println("Pair with minimum product: " + num1 + " " + num2);
+        System.out.println("Minimum product = " + minProduct);
+    }
+}
