c80927cf3e101ba56a147cee3a08814ec10fc8e6
rakeshbm7019-hue <rakeshbm.7019@gmail.com>
2026-08-31 20:42:16 +0530

    18th commit

---
+++ ArrayList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class ArrayList{
+    public static void main(String[]args){
+        java.util.ArrayList<Integer> list = new java.util.ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        int element = list.get(1);
+        System.out.println(element);
+
+        list.add(1,4);
+        System.out.println(list);
+
+        list.remove(2);
+        System.out.println(list);
+    }
+}
