89332bca92069d0393e3ada291366335e451098c
rakeshbm7019-hue <rakeshbm.7019@gmail.com>
2026-09-01 19:20:20 +0530

    21th commit

--- LCM.java
+++ LCM.java
@@ -1,17 +1,14 @@
 public class LCM {
     public static void main(String[] args) {
-        System.out.println("Enter two numbers to find their LCM:");
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int a = 12;
+        int b = 18;
 
         int gcd = findGCD(a, b);
         int lcm = (a * b) / gcd;
-          System.out.println("GCD = " + gcd);
+
         System.out.println("LCM = " + lcm);
     }
-      System.out.println("GCD = " + gcd);
-        System.out.println("LCM = " + lcm);
+
     static int findGCD(int x, int y) {
         while (y != 0) {
             int temp = y;
