bfc5611ecabec1ca783092c0390093753fdb3d7b
Rakesh B M <rakeshbm.7019@gmail.com>
2026-09-01 18:54:35 +0530

    Add Car class and main method for driving simulation

---
+++ OOPDemo.java
@@ -0,0 +1,23 @@
+class Car {
+    String brand;
+    int year;
+
+    Car(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    void drive() {
+        System.out.println(brand + " is driving!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", 2020);
+        Car car2 = new Car("Honda", 2022);
+
+        car1.drive();
+        car2.drive();
+    }
+}
