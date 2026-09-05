d9617953f76746cd09a685531f8e19b614365958
rakeshbm7019-hue <rakeshbm.7019@gmail.com>
2026-09-05 19:06:08 +0530

    commit

---
+++ addsub.java
@@ -0,0 +1,8 @@
+public class addsub {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+    }
+}
